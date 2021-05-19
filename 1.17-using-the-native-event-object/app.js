ce544bba9952cce89ebb89210eb2beecb626bc6b
Pablo Garcia <pablo.garcia2chromeburner.com>
2021-05-19 12:24:09 +0200

    2. 19 | Introducing computed properties.

--- 1.17-using-the-native-event-object/app.js
+++ 1.17-using-the-native-event-object/app.js
@@ -5,6 +5,15 @@ const app = Vue.createApp({
       name: ''
     };
   },
+  computed: {
+    fullname () {
+      console.log('running again')
+      if(this.name === '') {
+        return ''
+      }
+      return this.name + ' ' + 'Garcia'
+    }
+  },
   methods: {
     outputFullName () {
       console.log('running again')
@@ -28,10 +37,5 @@ const app = Vue.createApp({
     }
   }
 });
-
-/** In this example with methods when pushing buttons since vue does not know  what the method outputFullName() does it also refreshes this part.
- *  That is why in next lessons we will do this diffrently.
- * 
-*/
 
 app.mount('#events');
