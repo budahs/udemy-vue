df22a354dcd31cecd0637b6c83c4ee57c09c1619
Pablo Garcia <pablo.garcia2chromeburner.com>
2021-05-20 11:43:14 +0200

    5 | Behind the scenes | Vue javaascript proxy concept for reactive app.

---
+++ 5.behind-scenes/app.js
@@ -0,0 +1,44 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
+
+app.mount('#app');
+
+
+// ....
+
+const data = {
+  messagge: 'Hello!',
+  longMessage: 'hello ! World!'
+}
+
+const handler = {
+  set (target, key, value) {
+    // console.log(target)
+    // console.log(key)
+    // console.log(value)
+    if(key === 'message'){
+      target.longMessage = value + 'World'
+    }
+    target.message = value
+  }
+}
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = 'Hello!!!!!!!!!!'
+
+console.log(proxy.longMessage)
