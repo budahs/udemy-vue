725f98b01e75bf4014004275a68b9983fca004cc
Pablo Garcia <pablo.garcia2chromeburner.com>
2021-05-20 10:12:25 +0200

    4.4 | Monster Game | Finishing the core functionality

--- 4.-monster-game-project/app.js
+++ 4.-monster-game-project/app.js
@@ -14,9 +14,15 @@ const app = Vue.createApp({
     },
     computed: {
         monsterBarStyle () {
+            if(this.monsterHealth < 0){
+                return {width: '0%'}
+            }
             return {width: this.monsterHealth + '%'}
         },
         playerBarStyle () {
+            if(this.playerHealth < 0){
+                return {width: '0%'}
+            }
             return {width: this.playerHealth + '%'}
         },
         mayUseSpecialAttack () {
@@ -68,6 +74,15 @@ const app = Vue.createApp({
                 this.playerHealth += healValue
             }
             this.attackPlayer()            
+        },
+        startGame () {
+            this.playerHealth = 100
+            this.monsterHealth = 100
+            this.winner = null
+            this.currentRound = 0
+        },
+        surrender () {
+            this.winner = 'monster'
         }
     }
 })
