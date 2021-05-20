c7b77edc8b49b84cca5afb64a48b28e15bbf5345
Pablo Garcia <pablo.garcia2chromeburner.com>
2021-05-20 09:27:19 +0200

    4.4 | Monster Game | Adding heal functionality

--- 4.-monster-game-project/app.js
+++ 4.-monster-game-project/app.js
@@ -19,7 +19,7 @@ const app = Vue.createApp({
             return {width: this.playerHealth + '%'}
         },
         mayUseSpecialAttack () {
-            return currentRound % 3 !== 0
+            return this.currentRound % 3 !== 0
         }
     },
     methods: {
@@ -38,6 +38,17 @@ const app = Vue.createApp({
             const attackValue = getRandomValue(10, 25)
             this.monsterHealth -= attackValue
             this.attackPlayer() 
+        },
+        healPlayer () {
+            this.currentRound++
+            const healValue = getRandomValue(8, 20)
+            if(this.playerHealth + healValue > 100)
+            {
+                this.playerHealth = 100;
+            }else{
+                this.playerHealth += healValue
+            }
+            this.attackPlayer()            
         }
     }
 })
