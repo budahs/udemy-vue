64a44128b46ac4072cbe8863209b6e07f5dcdc83
Pablo Garcia <pablo.garcia2chromeburner.com>
2021-05-20 11:07:38 +0200

    4.4 | Monster Game | Adding a battle log

--- 4.-monster-game-project/app.js
+++ 4.-monster-game-project/app.js
@@ -9,7 +9,8 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound: 0,
-            winner: null
+            winner: null,
+            logMessages: []
         }
     },
     computed: {
@@ -52,11 +53,13 @@ const app = Vue.createApp({
             this.currentRound++
             const attackValue = getRandomValue(5, 12)
             this.monsterHealth -= attackValue
+            this.addLogMessage('player', 'attack', attackValue)
             this.attackPlayer()           
         },
         attackPlayer () {
             const attackValue = getRandomValue(8, 15)
-            this.playerHealth -= attackValue            
+            this.playerHealth -= attackValue 
+            this.addLogMessage('monster', 'attack', attackValue)           
         },
         specialAttackMonster () {
             this.currentRound++
@@ -73,6 +76,7 @@ const app = Vue.createApp({
             }else{
                 this.playerHealth += healValue
             }
+            this.addLogMessage('player', 'heal', healValue)
             this.attackPlayer()            
         },
         startGame () {
@@ -80,9 +84,17 @@ const app = Vue.createApp({
             this.monsterHealth = 100
             this.winner = null
             this.currentRound = 0
+            this.logMessages = []
         },
         surrender () {
             this.winner = 'monster'
+        },
+        addLogMessage(who, what, value) {
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            })
         }
     }
 })
