9e45062141e985973a837f5b3fcff11a91abd057
Pablo Garcia <pablo.garcia2chromeburner.com>
2021-05-25 10:26:48 +0200

    6.3 | Introducing components | Basic component concept

--- 6.introducing-components/app.js
+++ 6.introducing-components/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data () {
         return {
-            detailsAreVisible: false,
             friends: [                
                 {
                     id: 'manuel',
@@ -17,12 +16,37 @@ const app = Vue.createApp({
                 }
             ]
         }
+    }
+})
+
+app.component('friends-contact', {
+    template: `
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+    `,
+    data () {
+       return {
+           detailsAreVisible: false,
+           friend : 
+           {
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '01234 5678 991',
+                email: 'manuel@localhost.com'
+            }
+       } 
     },
     methods: {
         toggleDetails () {
             this.detailsAreVisible = !this.detailsAreVisible
         }
     }
-})
+});
 
 app.mount('#app')
