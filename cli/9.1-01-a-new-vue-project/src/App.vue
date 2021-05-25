028fe45aa05533c12d115faf982aa48fa428a813
Pablo Garcia <pablo.garcia2chromeburner.com>
2021-05-25 13:09:08 +0200

    7.1-11 | Moving to CLI | Component instal and first setup with cli

---
+++ cli/9.1-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <h2>My friend</h2>      
+        <ul>
+            <friend-contact></friend-contact>
+            <friend-contact></friend-contact>
+        </ul>
+    </section>    
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                friends: [
+                    {
+                        id: 'manuel',
+                        name: 'Manuel Lorenz',
+                        phone: '0123 45678 90',
+                        email: 'manuel@localhost.com' 
+                    },
+                    {
+                        id: 'julie Jones',
+                        name: '',
+                        phone: '0987 654421 21',
+                        email: 'julie@localhost.com' 
+                    }
+                ]
+            }
+        }        
+    }
+</script>
