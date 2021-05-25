4982fc641145b3becfd37c8581f5dcd40b56da6c
Pablo Garcia <pablo.garcia2chromeburner.com>
2021-05-25 18:01:55 +0200

    8.10 | Component communication | Adding components and connecting them

--- cli/9.1-01-a-new-vue-project/src/App.vue
+++ cli/9.1-01-a-new-vue-project/src/App.vue
@@ -1,6 +1,11 @@
 <template>
     <section>
-        <header><h1>My friend</h1></header>        
+        <header>
+            <h1>My friend</h1>
+        </header>        
+        <new-friend @add-contact="addContact">
+
+        </new-friend>
         <ul>
             <friend-contact
                 v-for="friend in friends"
@@ -45,6 +50,16 @@
                     friend => friend.id === friendId
                     )
                 identifiedFriend.isFavorite = !identifiedFriend.isFavorite
+            },
+            addContact (name, phone, email) {
+                const newFriendContact = {
+                    id: new Date().toISOString(),
+                    name: name,
+                    phone: phone,
+                    email: email,
+                    isFavorite: false
+                }
+                this.friends.push(newFriendContact)
             }
         }        
     }
@@ -82,7 +97,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -99,6 +115,7 @@ header {
   margin: 0 0 1rem 0;
 }
 
+
 #app button {
   font: inherit;
   cursor: pointer;
@@ -114,5 +131,18 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
