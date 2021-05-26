aa1cc7098f9ecc1d2a85fccaccca1ca81324313e
Pablo Garcia <pablo.garcia2chromeburner.com>
2021-05-26 15:32:03 +0200

    9.6-8 | Diving deeper into components | using named slots, shortcut for v-slot:, default slot content

--- cli/9.diving-deeper-into-components/src/components/UserInfo.vue
+++ cli/9.diving-deeper-into-components/src/components/UserInfo.vue
@@ -1,11 +1,13 @@
 <template>
   <section>
     <base-card>
-      <header>
+      <template #header>
         <h3>{{ fullName }}</h3>
         <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-      </header>
-      <p>{{ infoText }}</p>
+      </template>
+      <template #default>
+        <p>{{ infoText }}</p>
+      </template>
     </base-card>    
   </section>
 </template>
@@ -15,19 +17,3 @@ export default {
   props: ['fullName', 'infoText', 'role'],
 };
 </script>
-
-<style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
-section div {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-</style>
