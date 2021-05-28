6c167d63d43069d525c7e71a18831a9978460e6f
Pablo Garcia <pablo.garcia2chromeburner.com>
2021-05-28 11:45:38 +0200

    11.5 | Forms | Checkboxes and radio buttons.

--- cli/11.forms/src/components/TheForm.vue
+++ cli/11.forms/src/components/TheForm.vue
@@ -19,31 +19,37 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input id="interest-news" name="interest" type="checkbox" value="news" v-model="interest" />
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input id="interest-tutorials" name="interest" type="checkbox" value="tutorials" v-model="interest" />
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input id="interest-nothing" name="interest" type="checkbox" value="nothing" v-model="interest" />
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input id="how-video" name="how" type="radio" value="video" v-model="how" />
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input id="how-blogs" name="how" type="radio" value="blogs" v-model="how" />
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input id="how-other" name="how" type="radio" value="other" v-model="how" />
         <label for="how-other">Other</label>
+      </div>
+    </div>
+    <div class="form-control">
+      <div>
+        <input id="confirm-terms" name="confirm-terms" type="checkbox" v-model="confirm" />
+        <label for="confirm-terms">Agree terms of use?</label>
       </div>
     </div>
     <div>
@@ -58,7 +64,10 @@ export default {
     return {
       userName: '',
       userAge: null,
-      referrer: ''
+      referrer: '',
+      interest: [],
+      how:null,
+      confirm: false
     }
   },
   methods: {
@@ -73,6 +82,12 @@ export default {
         this.userAge = null        
         console.log(this.referrer + ' referrer')
         this.referrer = 'wom'
+        console.log('Checkboxes: ' + this.interest)
+        console.log('Radio: ' + this.how)
+        this.interest = []
+        this.how = null
+        console.log('Confirm: ' + this.confirm)
+        this.confirm = false
      } 
   }
 }
